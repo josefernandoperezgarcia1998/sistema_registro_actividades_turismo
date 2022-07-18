ced1126ac6d4471b427e36b3b39a3522ab6db5c0
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-07-18 09:55:54 -0500

    Vistas de errores, 403 y 404

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.general')
+
+@section('title_page', 'No autorizado - HTTP 403')
+
+@section('content_page')
+<div class="card">
+    <div class="card-body">
+        No tiene permiso para acceder a este apartado.
+    </div>
+</div>
+@endsection
+
+@push('css')
+
+@endpush
+
+@push('js')
+
+@endpush
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.general')
+
+@section('title_page', 'Página no encontrada - HTTP 404')
+
+@section('content_page')
+<div class="card">
+    <div class="card-body">
+        Esta página no existe.
+    </div>
+</div>
+@endsection
+
+@push('css')
+
+@endpush
+
+@push('js')
+
+@endpush
